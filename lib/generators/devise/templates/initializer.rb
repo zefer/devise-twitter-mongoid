324dd203130438a25df1cf4c5905f6e3055df993
Joe Roberts <joe@zefer.co.uk>
2011-02-27 19:45:21 +0000

    updated initializer to reflect new module path. Migration not needed for Mongoid

--- lib/generators/devise/templates/initializer.rb
+++ lib/generators/devise/templates/initializer.rb
@@ -1,4 +1,4 @@
-Devise::Twitter.setup do |config|
+Devise::Twitter::Mongoid.setup do |config|
   config.consumer_key = <YOUR CONSUMER KEY>
   config.consumer_secret = <YOUR CONSUMER SECRET>
   config.scope = :<%= singular_name %>
